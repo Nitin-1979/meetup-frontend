0d51a19724ccb1b3d71569b3c2016ae5f53027da
nitin kumar <nitinkumar010520@gmail.com>
2024-05-09 10:36:39 +0530

    push global api call config

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,15 +2,33 @@
 import Button from '@mui/material/Button';
 import ApiService from '../../services/apiService';
 import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 function Login() {
   const [userName, setUserName] = useState();
   const [password, setPassword] = useState();
-  const handleLoginClick = () =>{
-    console.log(userName,password);
-    let url = ApiService("user","user","get");
-    console.log(url);
+  let response = {};
+  let statusCode;
+  const navigate = useNavigate();
+  
+  async function fetchData() {
+    try {
+        let res = await ApiService("user", "user", "get");
+        response = res;
+        response.statusCode =  res.status;
+        response.data =  res.data;
+    } catch (error) {
+        console.error(error);
+    }
+  }
+  const handleLoginClick = async () => {
+    await fetchData();
+    if(response.statusCode == 200){
+      navigate('/home');
+    }else{
+      navigate('/signup')
+    }
   }
   
   return (
@@ -30,7 +48,7 @@ function Login() {
             value={password} onChange={(event)=>{ setPassword(event.target.value)}}
           />
         </div>
-        <div className='flex justify-center mt-3.5'>
+        <div className='flex justify-center mt-3.5'>     
           <Button variant="contained" onClick={handleLoginClick}>Login</Button>
         </div>
       </div>
